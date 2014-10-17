633b196425a1d5b7409c33fbdff0cc2dc754c758
Gregory Thomas Nisbet <gregory.nisbet@gmail.com>
2014-10-16 18:48:23 -0700

    added yeoman scaffolding

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('coralWeaponApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
