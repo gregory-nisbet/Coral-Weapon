6bcd475c90fc3e7fc23644ca58d1ce5e1ca2dcaf
Satoko Lom <stogamori@gmail.com>
2014-10-24 17:55:28 -0700

    (bug) new route button works with function gen Loop

--- slush/client/app/map.js
+++ slush/client/app/map.js
@@ -1,7 +1,7 @@
 var rendererOptions = {
   draggable: true
 };
-var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);;
+var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
 var directionsService = new google.maps.DirectionsService();
 var elevator;
 var map;
@@ -143,20 +143,8 @@ var getRequest = function(currentPo, destPo, wayPtArr){
   return request; 
 }
 
-var getRandomDest = function(){
-  var random = Math.random() / 100;
-  var randomDest = 0; 
-  if(random < 0.05){
-    randomDest = random; 
-  } else {
-    randomDest = 0.05; 
-  }
-  return randomDest;
-}
-
 var calcRoute = function() {
-  var randomDest = getRandomDest(); 
-  var destination = new google.maps.LatLng(currentLatitude+randomDest, currentLongitude+randomDest);
+  var destination = new google.maps.LatLng(currentLatitude, currentLongitude+0.02);
   var request = getRequest(currentPosition, destination);
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
@@ -197,32 +185,3 @@ var computeTotalDistance = function(result) {
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
-$(document).ready(function(){
-  $('.save_path').on('click', savePath);
-  $('#gen-route').on('click', genLoop);
-});
-
-var genLoop = function(){
-  var wayPtArr = []; 
-  var randomDest = getRandomDest(); 
-  var pt1 = new google.maps.LatLng(currentLatitude, currentLongitude+randomDest); 
-  var pt2 = new google.maps.LatLng(pt1.k, pt1.B+0.001); 
-  var pt3 = new google.maps.LatLng(pt2.k-0.002, pt2.B-0.005); 
-  wayPtArr.push({
-    location: pt1, 
-    stopover: true
-  }, {
-    location: pt2, 
-    stopover: true
-  },{
-    location: pt3, 
-    stopover: true
-  })
-  var request = getRequest(currentPosition, currentPosition, wayPtArr);
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-      drawChart(response);
-    }
-  });
-} 
